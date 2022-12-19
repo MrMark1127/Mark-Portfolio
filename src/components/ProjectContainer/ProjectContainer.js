d456177701dd35b469566417cf6d3aec16d58c5a
MrMark1127 <mark11273@gmail.com>
2022-12-19 17:03:54 -0500

    Chores | cleanup | bug fixing

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -7,8 +7,8 @@ function ProjectContainer(props) {
     return (
         
             <div className={["text-center m-3 p-2 rounded-md flex flex-col justify-center items-center bg-teal/[0.13]", hoverStyle].join(" ")}>
-            <a className="h-3/4 pb-2" href={props.anchorLink} target="_blank" rel="noreferrer">
-                    <img className={["mt-3 3xl:mt-10 px-1 h-full rounded-sm", props.classProps].join(" ")} src={props.imgSrc} alt="" /></a>
+            <a className="h-3/4 pb-2" target="_blank" rel="noopener noreferrer" href={props.anchorLink}>
+                    <img className={["mt-3 3xl:mt-10 px-1 3xl:h-64 rounded-sm drop-shadow-xl", props.classProps].join(" ")} src={props.imgSrc} alt="" /></a>
                     <h1 className="my-3 3xl:my-5 text-xs sm:text-md lg:text-lg 3xl:text-2xl">{props.skillName}</h1>
             </div>
     )
--- src/components/SideBar/LeftSideBar.js
+++ src/components/SideBar/LeftSideBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import SocialButtons from "../SocialButtons/SocialButtons";
+import {Link} from "react-router-dom"
 
 function LeftSideBar(props) {
     //BORDER FOR SIDE BARS border-dashed border-2 border-black 
     return (
         <div className="text-white flex flex-col overflow-hidden hidden md:flex md:col-span-1">
-            <p className="flex-1 mt-4 3xl:mt-12 font-DancingScript text-4xl lg:text-5xl 3xl:text-6xl"><a href="/">Donahue Solutions</a></p>
+            <p className="flex-1 mt-4 3xl:mt-12 font-DancingScript text-4xl lg:text-5xl 3xl:text-6xl"><Link to="/">Donahue Solutions</Link></p>
             <SocialButtons 
                 socialNetwork="faGithub"
                 borderRadius="hover:rounded-full"
